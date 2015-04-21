c0d9345b39e90dc5cad631af27a0f7c7ce1b81fc
Ricarda Schueler <ricarda.schueler@gmx.de>
2015-04-21 16:00:06 +0200

    calculate all for nodeHistogram

--- nodeHistogramJava/nodeHistogramJava/src/main/java/de/hpi/uni_potsdam/de/nodeHistogramJava/NodeHistogramm.java
+++ nodeHistogramJava/nodeHistogramJava/src/main/java/de/hpi/uni_potsdam/de/nodeHistogramJava/NodeHistogramm.java
@@ -1,47 +1,53 @@
 package de.hpi.uni_potsdam.de.nodeHistogramJava;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.SparkConf;
 
 import scala.Tuple2;
 
-
-
-
-
 public class NodeHistogramm {
 	
 	static String SPLITCHARACTER = "\t";
 	static String outputFollower = "output/follower";
 	static String outputFollowing = "output/following";
+	static String outputAll = "output/all";
 	
 	public static void main(String[] args) {
 		SparkConf conf = new SparkConf().setAppName("de.hpi.uni_potsdam.de.nodeHistogramJava.NodeHistogram");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		File outputFollowerFile = new File(outputFollower);
 		File outputFollowingFile = new File(outputFollowing);
+		File outputAllFile = new File(outputAll);
 		try {
 			FileUtils.deleteDirectory(outputFollowerFile);
 			FileUtils.deleteDirectory(outputFollowingFile);
+			FileUtils.deleteDirectory(outputAllFile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		spark(sc, 0, outputFollower);
-		spark(sc, 1, outputFollowing);
+		//follower
+		JavaPairRDD<String, Integer> followerCount =  calculateCounts(sc, 0);
+		followerCount.saveAsTextFile(outputFollower);
+		
+		//following
+		JavaPairRDD<String, Integer> followingCount = calculateCounts(sc, 1);
+		followerCount.saveAsTextFile(outputFollowing); 
+		
+		//all
+		JavaPairRDD<String, Integer> follow = followingCount.union(followerCount);
+		JavaPairRDD<String, Integer> count = sum(follow);
+		count.saveAsTextFile(outputAll); 
 	}
 
-	public static void spark(JavaSparkContext sc, final int type, String outputLocation){
-
+	public static JavaPairRDD<String, Integer> calculateCounts(JavaSparkContext sc, final int type){
 		JavaRDD<String> lines = sc.textFile("../smallTwitter.txt");
 
 		JavaPairRDD<String, Integer> followerPairs = lines.mapToPair(new PairFunction<String, String, Integer>() {
@@ -50,11 +56,20 @@ public class NodeHistogramm {
 			  }
 		});
 		
-		JavaPairRDD<String, Integer> followerCount = followerPairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
-		  public Integer call(Integer a, Integer b) { return a + b; }
-		});
+		JavaPairRDD<String, Integer> followerCount = sum(followerPairs);
 		
-		followerCount.saveAsTextFile(outputLocation); 
+		return followerCount;
 	}
+	
+	public static JavaPairRDD<String, Integer> sum (JavaPairRDD<String, Integer> followPairs){
+		return followPairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+			  public Integer call(Integer a, Integer b) { return a + b; }
+			});
+	}
+	
+
+
+	
+	
 	
 }
