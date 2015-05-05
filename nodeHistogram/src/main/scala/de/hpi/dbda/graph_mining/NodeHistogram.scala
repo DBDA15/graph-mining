13b0362f1ae4fd990d22bcdeb497e67dc7a4ae5b
Ricarda Schueler <ricarda.schueler@gmx.de>
2015-05-05 13:33:48 +0200

    refactor

--- nodeHistogram/src/main/scala/de/hpi/dbda/graph_mining/NodeHistogram.scala
+++ nodeHistogram/src/main/scala/de/hpi/dbda/graph_mining/NodeHistogram.scala
@@ -17,48 +17,61 @@ object NodeHistogram extends App {
 
   //print("hello World")
 
+  @Override
+  override def main(args: Array[String]) {
+    val inputPath = args(0)
+    calculateIncomingOutcomingCount(inputPath, args)
+  }
 
-  def main(): Unit ={
-
+  def convertToBidirectedGraph(inputPath: String): Unit ={
+    
 
   }
-  val inputPath = args(0)
-  val followerPath = args(1)
-  val followingPath = args(2)
-  val combinedPath = args(3)
 
-  val conf = new SparkConf()
-  conf.setAppName(NodeHistogram.getClass.getName)
-  conf.set("spark.hadoop.validateOutputSpecs", "false");
-  val context = new SparkContext(conf)
 
-  val twitterEntries =
-    context.textFile(inputPath)
-      .flatMap(line => List(
-          new TwitterEntry(line.split("\t")(0),0).asTuple,
-          new TwitterEntry(line.split("\t")(1),1).asTuple))
 
-  val relationCountEntries = twitterEntries.reduceByKey((entry1, entry2) =>
+
+  def calculateIncomingOutcomingCount(inputPath:String, args: Array[String]): Unit = {
+
+    val followerPath = args(1)
+    val followingPath = args(2)
+    val combinedPath = args(3)
+
+    val conf = new SparkConf()
+    conf.setAppName(NodeHistogram.getClass.getName)
+    conf.set("spark.hadoop.validateOutputSpecs", "false");
+    val context = new SparkContext(conf)
+
+    val twitterEntries =
+      context.textFile(inputPath)
+        .flatMap(line => List(
+        new TwitterEntry(line.split("\t")(0),0).asTuple,
+        new TwitterEntry(line.split("\t")(1),1).asTuple))
+
+    val relationCountEntries = twitterEntries.reduceByKey((entry1, entry2) =>
       new TwitterEntry(
         entry1.id,
         entry1.followerCount + entry2.followerCount,
         entry1.followingCount + entry2.followingCount))
 
-  // calculate followerCount
-  val followerCount = relationCountEntries.map(twitterEntry => (twitterEntry._2.followerCount, 1))
+    // calculate followerCount
+    val followerCount = relationCountEntries.map(twitterEntry => (twitterEntry._2.followerCount, 1))
       .reduceByKey((f1, f2) => f1 + f2)
 
-  followerCount.saveAsTextFile(followerPath)
+    followerCount.saveAsTextFile(followerPath)
 
-  //calculate followingCount
-  val followingCount = relationCountEntries.map(twitterEntry => (twitterEntry._2.followingCount, 1))
+    //calculate followingCount
+    val followingCount = relationCountEntries.map(twitterEntry => (twitterEntry._2.followingCount, 1))
       .reduceByKey((f1, f2) => f1 + f2)
 
-  followingCount.saveAsTextFile(followingPath)
+    followingCount.saveAsTextFile(followingPath)
 
-  //calculate combinedCount
-  val combinedCount = relationCountEntries.map(twitterEntry => (twitterEntry._2.followingCount + twitterEntry._2.followerCount, 1))
-    .reduceByKey((f1, f2) => f1 + f2)
+    //calculate combinedCount
+    val combinedCount = relationCountEntries.map(twitterEntry => (twitterEntry._2.followingCount + twitterEntry._2.followerCount, 1))
+      .reduceByKey((f1, f2) => f1 + f2)
 
-  combinedCount.saveAsTextFile(combinedPath)
+    combinedCount.saveAsTextFile(combinedPath)
+    //val test = combinedCount.take(30)
+
+  }
 }
