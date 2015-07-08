cc69259c9f5bf7ad21459b410af7e4a06d0996b7
Ricarda Schueler <ricarda.schueler@gmx.de>
2015-07-08 16:35:39 +0200

    other benchmark configs

--- graph-mining-flink/benchmarks.sh
+++ graph-mining-flink/benchmarks.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-program="cat test"
-
-
 declare -A configurations
 
 configurations=(
-	["TrussK5C20"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 20 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 5"
-	["TrussK5C10"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 10 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 5"
-	["TrussK5C5"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 5 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 5"
-	["TrussK5C2"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 2 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 5"
-	["TrussK5C1"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 1 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 5"
+	["TrussK5C20"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 20 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 20"
+	["TrussK5C10"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 10 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 20"
+	["TrussK5C5"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 5 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 20"
+	["TrussK5C2"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 2 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 20"
+	["TrussK5C1"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 1 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 20"
 	["TrussK10C20"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 20 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 10"
 	["TrussK3C20"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 20 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 3"
-	["TrussK20C20"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 20 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 20"
 	["TrussK40C20"]="/opt/flink/flink-0.9.0/bin/flink run --class de.hpi.dbda.graph_mining.GraphMiningFlink  --parallelism 20 -m tenemhead2:6123 target/graph-mining-flink-1.0-SNAPSHOT.jar truss hdfs://tenemhead2/data/graph-mining/biDirectionalWiki hdfs://tenemhead2/home/ricarda.schueler/output/ \t 40"
 )
 
@@ -22,6 +18,7 @@ for config in "${!configurations[@]}"; do
 	echo "====================================" >> time.log
 	date "+%Y-%m-%d %H:%M:%S" >> time.log
 	echo "${configurations["$config"]} - output in output_$config.log" >> time.log
+	echo $config
 	(time ${configurations["$config"]} >> "output_$config.log") &>> time.log
 	echo "====================================" >> time.log
 	echo "" >> time.log
